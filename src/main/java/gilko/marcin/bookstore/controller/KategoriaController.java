6e351070b75239ac7c0d15f810f5595ccebae37c
gilkom <gilek06@op.pl>
2020-07-01 22:23:16 +0200

    adding kategoriaController

--- src/main/java/gilko/marcin/bookstore/controller/KategoriaController.java
+++ src/main/java/gilko/marcin/bookstore/controller/KategoriaController.java
@@ -1,5 +1,13 @@
 package gilko.marcin.bookstore.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import gilko.marcin.bookstore.service.KategoriaService;
+
+@Controller
 public class KategoriaController {
 
+	@Autowired
+	private KategoriaService service;
 }
