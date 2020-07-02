b61d7e0563f351c7a93e3c7e9ead7efc3edbd8e1
gilkom <gilek06@op.pl>
2020-07-02 12:42:30 +0200

    adding mapping to bk_kategoria model

--- src/main/java/gilko/marcin/bookstore/model/Bk_kategoria.java
+++ src/main/java/gilko/marcin/bookstore/model/Bk_kategoria.java
@@ -1,34 +1,42 @@
 package gilko.marcin.bookstore.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "BK_KATEGORIA")
 public class Bk_kategoria {
+
 	private int id;
-	private String name;
+	private String nazwa_kategorii;
 	private String description;
 	
 	public Bk_kategoria() {
 	}
-	public Bk_kategoria(int id, String name, String description) {
+	public Bk_kategoria(int id, String nazwa_kategorii, String description) {
 		this.id = id;
-		this.name = name;
+		this.nazwa_kategorii = nazwa_kategorii;
 		this.description = description;
 	}
 	@Id
+	@Column(name = "ID_KATEGORII")
 	public int getId() {
 		return id;
 	}
+	
 	public void setId(int id) {
 		this.id = id;
 	}
+	@Column(name = "NAZWA_KATEGORII")
 	public String getName() {
-		return name;
+		return nazwa_kategorii;
 	}
-	public void setName(String name) {
-		this.name = name;
+	public void setName(String nazwa_kategorii) {
+		this.nazwa_kategorii = nazwa_kategorii;
 	}
+	@Column(name = "OPIS_KATEGORII")
 	public String getDescription() {
 		return description;
 	}
