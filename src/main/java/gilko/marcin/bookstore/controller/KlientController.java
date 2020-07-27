abe389b9599a1b64d246cc4e35ab9c7256d824da
gilkom <gilek06@op.pl>
2020-07-27 20:01:44 +0200

    adding spring security

--- src/main/java/gilko/marcin/bookstore/controller/KlientController.java
+++ src/main/java/gilko/marcin/bookstore/controller/KlientController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -42,6 +43,10 @@ public class KlientController {
 		if(bindingResult.hasErrors()) {
 			return "nowy_klient";
 		}else {
+			BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+			String rawPass = klient.getPassword();
+			String encodedPass = encoder.encode(rawPass);
+			klient.setPassword(encodedPass);
 			service.save(klient);
 			return "redirect:/lista_klientow";
 		}
