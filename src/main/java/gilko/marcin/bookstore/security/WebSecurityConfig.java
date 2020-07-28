d69457b34b550922329065398a288db34e92b99b
gilkom <gilek06@op.pl>
2020-07-28 19:37:10 +0200

    adding spring security

--- src/main/java/gilko/marcin/bookstore/security/WebSecurityConfig.java
+++ src/main/java/gilko/marcin/bookstore/security/WebSecurityConfig.java
@@ -1,20 +1,31 @@
-/*package gilko.marcin.bookstore.security;
+package gilko.marcin.bookstore.security;
 
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-
 @Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+ 
+    @Autowired
+    private DataSource dataSource;
+     
+    @Autowired
+    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().passwordEncoder(new BCryptPasswordEncoder())
+            .dataSource(dataSource)
+            .usersByUsernameQuery("select email_klienta as username, password as password,blokada_konta as enabled from bk_klient where email_klienta=?")
+            .authoritiesByUsernameQuery("select email_klienta as username, rola as role from bk_klient where email_klienta=?")
+        ;
+    }
+/*@Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 	
@@ -25,10 +36,21 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 	public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception{
 		auth.jdbcAuthentication().passwordEncoder(new BCryptPasswordEncoder())
 		.dataSource(dataSource)
-		.usersByUsernameQuery("select email_klienta, password from bk_klient where email_klienta=?")
-		.authoritiesByUsernameQuery("select email_klienta, rola from bk_klient where email_klienta=?")
+		//.usersByUsernameQuery("select bk_klient.email_klienta as username, bk_klient.password  as password, 1 as enabled from bk_klient where bk_klient.email_klienta=?")
+		.usersByUsernameQuery("select username, password, 1 as enabled from userss where username=?")
+		//.authoritiesByUsernameQuery("select bk_klient.email_klienta, bk_klient.rola from bk_klient where bk_klient.email_klienta=?")
+		.authoritiesByUsernameQuery("select username, rola from userss where username =?")
 				;
 	}
+	*/
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+            .anyRequest().authenticated()
+            .and()
+            .formLogin().permitAll()
+            .and()
+            .logout().permitAll();     
+    }
 
 }
-*/
