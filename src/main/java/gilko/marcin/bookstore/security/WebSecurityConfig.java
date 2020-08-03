5d7a0229b2af78959c62d02940fb3055e690aeb3
gilkom <gilek06@op.pl>
2020-08-03 22:25:57 +0200

    adding spring security

--- src/main/java/gilko/marcin/bookstore/security/WebSecurityConfig.java
+++ src/main/java/gilko/marcin/bookstore/security/WebSecurityConfig.java
@@ -29,12 +29,29 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-        	.antMatchers("/","/lista_ksiazek","/wyswietl_ksiazke/*","/lista_ksiazek/page/*", "/login", "/images/*", "/rejestracja", "/rejestracja/save").permitAll()
+        	.antMatchers("/","/lista_ksiazek","/wyswietl_ksiazke/*",
+        			"/lista_ksiazek/page/{\\d+}/?sortField=[\\w+]&sortDir=[\\w+]&keyword=[\\w*]&nazwaKategorii=[\\w*]",
+        			"/login", "/images/*", "/rejestracja", "/rejestracja/save").permitAll()
+        	.antMatchers("/lista_kategorii", "/nowa_kategoria", "/nowa_kategoria/save", "/edytuj_kategorie/save", "/edytuj_kategorie/{\\d+}", "/usun_kategorie/{\\d+}").hasRole("ADMIN")
+        	.antMatchers("/lista_wydawnictw","/nowe_wydawnictwo", "/nowe_wydawnictwo/save", "/edytuj_wydawnictwo/save").hasRole("ADMIN")
+        	.antMatchers("/lista_autorow", "/nowy_autor", "/nowy_autor/save", "/edytuj_autora/save", "/usun_autora/save").hasRole("ADMIN")
+        	.antMatchers("/lista_uzytkownikow", "/nowy_uzytkownik", "/nowy_uzytkownik/save", "/edytuj_uzytkownika/{id}", "/edytuj_uzytkownika/save", "/usun_uzytkownika/{id}").hasRole("ADMIN")
+        	.antMatchers("/lista_opinii", "/nowa_opinia", "/nowa_opinia/save", "/edytuj_opinie/{id_uzytkownika}/{id_ksiazki}", "/edytuj_opinie/save", "/usun_opinie/{id_uzytkownika}/{id_ksiazki}").hasRole("ADMIN")
+        	.antMatchers("/lista_klientow").hasRole("OPERATOR")
+        	.antMatchers("/lista_zamowien_obsluga", "/wyswietl_zamowienie_obsluga/{id}").hasRole("OPERATOR")
+        	.antMatchers("/lista_zamowien_obsluga", "/wyswietl_zamowienie_obsluga/{id}").hasRole("OPERATOR")
+        	.antMatchers("/lista_zamowien_zamowione", "/wyswietl_zamowienie_zamowione/{id}", "/wyswietl_zamowienie_zamowione/skompletuj").hasRole("OPERATOR")
+        	.antMatchers("/lista_zamowien_skompletowane", "/wyswietl_zamowienie_skompletowane/{id}", "/wyswietl_zamowienie_skompletowane/wyslij").hasRole("OPERATOR")
+        	.antMatchers("/lista_zamowien_wyslane", "/wyswietl_zamowienie_wyslane/{id}", "/wyswietl_zamowienie_wyslane/zamknij").hasRole("OPERATOR")
+        	.antMatchers("/lista_zamowien_zamkniete", "/wyswietl_zamowienie_zamkniete/{id}").hasRole("OPERATOR")
+        	.antMatchers("/koszyk", "/wyswietl_zamowienie/{id}", "/edytuj_koszyk/{id_zamowienia}/{pozycja_zamowienia}").hasRole("USER")
+        	.antMatchers("/edytuj_koszyk/save","/usun_z_koszyka/{id_zamowienia}/{pozycja_zamowienia}","/moje_zamowienia").hasRole("USER")
+        	.antMatchers("/zloz_zamowienie","/edytuj_dane_adresowe","/edytuj_dane_adresowe/save").hasRole("USER")
             .anyRequest().authenticated()
             .and()
             .formLogin().loginPage("/login").permitAll()
             .and()
-            .logout().logoutSuccessUrl("/") ;   
+            .logout().logoutSuccessUrl("/").permitAll() ;   
     }
 
 }
